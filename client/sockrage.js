a5c2ad837844c950faa22239944783ef9e398c3f
NGUYEN Alexandre <alex.nr@hotmail.fr>
2014-07-22 10:20:35 +0900

    client alpha 1

--- client/sockrage.js
+++ client/sockrage.js
@@ -1,22 +1,21 @@
-
 function SockRage(_uri, _reference) {
 
-	this.uri = _uri;
-	this.reference = _reference;
-	this.socket = io(_uri);
+		this.uri = _uri;
+		this.reference = _reference;
+		this.socket = io(_uri);
 
-	this.set = function(data) {
+		this.set = function(data) {
 
-		var dataEntity = {
+			var dataEntity = {
 			operation : "create",
 			reference : this.reference,
 			datas : data
-		};
+			};
 
-		this.socket.emit('reference-listening', dataEntity);
-	}
+			this.socket.emit('reference-listening', dataEntity);
+		}
 
-	this.get = function(_id) {
+		this.get = function(_id) {
 
 		var dataEntity = {
 			_id : _id,
@@ -24,21 +23,21 @@ function SockRage(_uri, _reference) {
 			reference : this.reference
 		}
 
-	this.socket.emit('reference-listening', dataEntity);
+		this.socket.emit('reference-listening', dataEntity);
 
-	}
+		}
 
-	this.list = function() {
+		this.list = function() {
 
 		var dataEntity = {
 			operation : "getAll",
 			reference : this.reference
 		}
 
-		this.socket.emit('reference-listening', dataEntity);
-	}
+			this.socket.emit('reference-listening', dataEntity);
+		}
 
-	this.delete = function(_id) {
+		this.delete = function(_id) {
 
 		var dataEntity = {
 			_id : _id,
@@ -46,32 +45,23 @@ function SockRage(_uri, _reference) {
 			reference : this.reference
 		}
 
-		this.socket.emit('reference-listening', dataEntity);
-	}
+			this.socket.emit('reference-listening', dataEntity);
+		}
 
-	this.update = function(_id, data) {
+		this.update = function(_id, data) {
 
-		var dataEntity = {
-			_id : _id,
+			var dataEntity = {
+				_id : _id,
 			operation : "update",
 			reference : this.reference,
 			datas : data
-		};
+			};
 
-		this.socket.emit('reference-listening', dataEntity);
-	}
+			this.socket.emit('reference-listening', dataEntity);
+		}
 
-	//TODO
-
-	this.on = function() {
-	  this.socket.on(this.reference, function (data) {
-	    	SockRage.listenerCallback(data);
-	  });
-	}
-
-	this.listenerCallback = function(data) {
-
-		console.log(data);
-	}
+		this.on = function(callback) {
+	  this.socket.on(this.reference, callback);
+		}
 
 }
